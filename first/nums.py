a0d1948948b99be5db78ba40936a5ec19f76b471
K-922 <hatebreader@yandex.ru>
2021-04-18 19:30:41 +0300

    Исправлены ошибки, отформатирован текст

--- first/nums.py
+++ first/nums.py
@@ -5,6 +5,12 @@ def plus(a, b):
 def minus(a, b):
     return a - b
 
+
+def multipl(a, b):
+    return a * b
+
+
 if __name__ == '__main__':
     print(plus(3, 2))
-    print((minus(7, 3))
+    print(minus(7, 3))
+    print(multipl(6, 4))
